831942a6e4d47c04f291de5757ec412163bbf94b
Abhishek Bhardwaj <40014349+abhiib02@users.noreply.github.com>
2024-12-19 22:48:47 +0530

    Fixed Bug When Input Multiple Style Tag Creates and lags HTML

--- ThemeColorChangerTool.js
+++ ThemeColorChangerTool.js
@@ -1,9 +1,9 @@
 function changeValueofcssVariable(cssVariable){
-  const style = document.createElement('style');
+  const style = document.querySelector(`#${cssVariable}`);
   const input = document.querySelector(`#${cssVariable}-input`);
-  style.id=`${cssVariable}`;
+  style.innerHTML ="";
   style.innerHTML = `:root{--${cssVariable}:${input.value};}`;
-  document.head.appendChild(style);
+  
 }
 function themeColorChanger(cssVarArr){
   if(!cssVarArr){
@@ -12,17 +12,23 @@ function themeColorChanger(cssVarArr){
   const form = document.createElement('form');
   
   const fragment = new DocumentFragment();
+  const fragmentStyle = new DocumentFragment();
   form.style="display:flex;flex-direction:column;position:fixed;top:0%;right:0%;z-index:10000;background:white;padding:10px;outline:1px solid #000;";
   cssVarArr.forEach((cssVariable)=>{
     const label = document.createElement('label');
     const input = document.createElement('input');
+    const style = document.createElement('style');
+    style.id=`${cssVariable}`;
     input.type="color";
     input.id=`${cssVariable}-input`;
     input.setAttribute('onInput',`changeValueofcssVariable('${cssVariable}')`);
     label.textContent = `--${cssVariable} : `;
     label.append(input);
     fragment.append(label);
+    fragmentStyle.append(style);
   })
   form.append(fragment);
+  document.head.appendChild(fragmentStyle);
   document.body.appendChild(form);
+   
 }
